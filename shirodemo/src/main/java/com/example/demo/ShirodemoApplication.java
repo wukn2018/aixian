fd9dccca5dce2b9a8bcf5a6a43349b8e3003d2b5
wukn2018 <wukn@xinguangnet.com>
2018-07-20 17:19:17 +0800

    shiro用例

--- shirodemo/src/main/java/com/example/demo/ShirodemoApplication.java
+++ shirodemo/src/main/java/com/example/demo/ShirodemoApplication.java
@@ -2,13 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.example.demo")
 @SpringBootApplication
-public class ShiroApplication {
+public class ShirodemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShiroApplication.class, args);
+		SpringApplication.run(ShirodemoApplication.class, args);
 	}
 }
